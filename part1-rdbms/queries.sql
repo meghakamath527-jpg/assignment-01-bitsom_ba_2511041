d23cb60b1ffb4a567e1fb76a0ea030f7768bf030
meghakamath527-jpg <meghakamath527@gmail.com>
2026-03-14 21:38:55 +0530

    update

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,10 +1,1 @@
--- Q1: List all customers from Mumbai along with their total order value
-SELECT c.customer_name,
-       SUM(p.unit_price * oi.quantity) AS total_order_value
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
-JOIN products p ON oi.product_id = p.product_id
-WHERE c.customer_city = 'Mumbai'
-GROUP BY c.customer_name;
 
